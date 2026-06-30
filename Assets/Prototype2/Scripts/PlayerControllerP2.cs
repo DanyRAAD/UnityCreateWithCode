dba6fa1508714aab41fe793a44a26bbc7af28906
DanyRAAD <ramirezandresdaniela@gmail.com>
2026-06-30 17:11:48 -0600

    bonus features

--- Assets/Prototype2/Scripts/PlayerControllerP2.cs
+++ Assets/Prototype2/Scripts/PlayerControllerP2.cs
@@ -6,7 +6,9 @@ public class PlayerControllerP2 : MonoBehaviour
     public InputAction moveAction;
     public Vector2 moveInput;
     public float speed = 10.0f;
-    public float xRange = 20.0f;
+    public float xRange = 15.0f;
+    public float zRange = 16.0f;
+    public float zMin = -1.0f;
 
     public GameObject projectilePrefab;
     public InputAction fireAction;    
@@ -29,7 +31,16 @@ public class PlayerControllerP2 : MonoBehaviour
             transform.position = new Vector3(xRange,transform.position.y,transform.position.z);
         }
 
+        if(transform.position.z < zMin)
+        {
+            transform.position = new Vector3(transform.position.x,transform.position.y,zMin);
+        }
+        if(transform.position.z > zRange)
+        {
+            transform.position = new Vector3(transform.position.x,transform.position.y,zRange);
+        }
         moveInput = moveAction.ReadValue<Vector2>();
+        transform.Translate(Vector3.forward * moveInput.y * Time.deltaTime * speed);
         transform.Translate(Vector3.right * moveInput.x * Time.deltaTime * speed);
 
         if (fireAction.triggered)
--- Assets/Prototype2/Scripts/SpawnManager.cs
+++ Assets/Prototype2/Scripts/SpawnManager.cs
@@ -1,22 +1,23 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 20f;
     private float spawnPosZ = 20f;
+
+    private float spawnRangeZ = 15f;
+    private float spawnPosX = 20f;
+
     private float startDelay = 2f;
     private float spawnInterval = 1.5f;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
-        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);   
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
-    // Update is called once per frame
     void Update()
     {
     }
@@ -24,7 +25,25 @@ public class SpawnManager : MonoBehaviour
     void SpawnRandomAnimal()
     {
         int animalIndex = Random.Range(0, animalPrefabs.Length);
+
+   
         Vector3 spawnPosition = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
         Instantiate(animalPrefabs[animalIndex], spawnPosition, animalPrefabs[animalIndex].transform.rotation);
+
+        
+        bool spawnLeft = Random.Range(0, 2) == 0;
+
+        Vector3 sideSpawnPosition;
+
+        if (spawnLeft)
+        {
+            sideSpawnPosition = new Vector3(-spawnPosX, 0, Random.Range(0, spawnRangeZ));
+            Instantiate(animalPrefabs[animalIndex], sideSpawnPosition, Quaternion.Euler(0, 90, 0));
+        }
+        else
+        {
+            sideSpawnPosition = new Vector3(spawnPosX, 0, Random.Range(0, spawnRangeZ));
+            Instantiate(animalPrefabs[animalIndex], sideSpawnPosition, Quaternion.Euler(0, -90, 0));
+        }
     }
 }
